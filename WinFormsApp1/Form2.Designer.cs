5cad1fb41d33057fa86e52408f6327fcb9fb07ad
vlad <vlad.s.o@yandex.ru>
2022-11-23 12:09:14 +0300

    tim

--- WinFormsApp1/Form2.Designer.cs
+++ WinFormsApp1/Form2.Designer.cs
@@ -28,57 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.up = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button9 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // up
             // 
-            this.button1.Location = new System.Drawing.Point(78, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(60, 60);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.up.Location = new System.Drawing.Point(78, 12);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(60, 60);
+            this.up.TabIndex = 0;
+            this.up.Text = "up";
+            this.up.UseVisualStyleBackColor = true;
+
+            this.up.MouseDown += new System.Windows.Forms.MouseEventHandler(this.up_MouseDown);
+            this.up.MouseUp += new System.Windows.Forms.MouseEventHandler(this.right_MouseUp);
             // 
-            // button2
+            // left
             // 
-            this.button2.Location = new System.Drawing.Point(12, 78);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(60, 60);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.left.Location = new System.Drawing.Point(12, 78);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(60, 60);
+            this.left.TabIndex = 1;
+            this.left.Text = "left";
+            this.left.UseVisualStyleBackColor = true;
+
+            this.left.MouseDown += new System.Windows.Forms.MouseEventHandler(this.left_MouseDown);
+            this.left.MouseUp += new System.Windows.Forms.MouseEventHandler(this.right_MouseUp);
             // 
-            // button3
+            // down
             // 
-            this.button3.Location = new System.Drawing.Point(78, 78);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(60, 60);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.down.Location = new System.Drawing.Point(78, 78);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(60, 60);
+            this.down.TabIndex = 2;
+            this.down.Text = "down";
+            this.down.UseVisualStyleBackColor = true;
+   
+            this.down.MouseDown += new System.Windows.Forms.MouseEventHandler(this.down_MouseDown);
+            this.down.MouseUp += new System.Windows.Forms.MouseEventHandler(this.right_MouseUp);
             // 
-            // button4
+            // right
             // 
-            this.button4.Location = new System.Drawing.Point(144, 78);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(60, 60);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.right.Location = new System.Drawing.Point(144, 78);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(60, 60);
+            this.right.TabIndex = 3;
+            this.right.Text = "right";
+            this.right.UseVisualStyleBackColor = true;
+
+            this.right.MouseDown += new System.Windows.Forms.MouseEventHandler(this.right_MouseDown);
+            this.right.MouseUp += new System.Windows.Forms.MouseEventHandler(this.right_MouseUp);
             // 
             // button5
             // 
@@ -139,6 +149,10 @@
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -150,10 +164,10 @@
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.up);
             this.KeyPreview = true;
             this.Name = "Form2";
             this.Text = "Form2";
@@ -166,15 +180,16 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+        private Button up;
+        private Button left;
+        private Button down;
+        private Button right;
         private Button button5;
         private Button button6;
         private Button button7;
         private Button button8;
         private RichTextBox richTextBox1;
         private Button button9;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- WinFormsApp1/Form2.cs
+++ WinFormsApp1/Form2.cs
@@ -13,33 +13,12 @@ namespace WinFormsApp1
 {
     public partial class Form2 : Form
     {
+        public bool f_right, f_left, f_up, f_down =false;
         public Form2()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Owner.Location = new Point( (Owner.Location.X), (Owner.Location.Y - 5));
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Owner.Location = new Point((Owner.Location.X), (Owner.Location.Y + 5));
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Owner.Location = new Point((Owner.Location.X - 5), (Owner.Location.Y ));
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Owner.Location = new Point((Owner.Location.X + 5), (Owner.Location.Y ));
-
-            Properties.Settings.Default.X = Owner.Location.X;
-            Properties.Settings.Default.Y = Owner.Location.Y;
-        }
 
         private void button5_Click(object sender, EventArgs e)
         {
@@ -71,7 +50,7 @@ namespace WinFormsApp1
             Properties.Settings.Default.Opacity = Owner.Opacity;
         }
 
-    
+
 
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -80,13 +59,13 @@ namespace WinFormsApp1
             {
                 e.Cancel = true;
             }
-          Hide();   
-            
+            Hide();
+
         }
 
         private void Form2_KeyDown(object sender, KeyEventArgs e)
         {
-            richTextBox1.Text=(Convert.ToString(e.KeyValue));
+            richTextBox1.Text = (Convert.ToString(e.KeyValue));
             //return;
         }
 
@@ -99,8 +78,70 @@ namespace WinFormsApp1
 
         private void Form2_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
-           // richTextBox1.AppendText(Convert.ToString(e.KeyValue));
+            // richTextBox1.AppendText(Convert.ToString(e.KeyValue));
 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (f_right)
+            {
+                Owner.Location = new Point((Owner.Location.X + 5), (Owner.Location.Y));
+            }
+            if (f_left)
+            {
+                Owner.Location = new Point((Owner.Location.X - 5), (Owner.Location.Y));
+            }
+            if (f_up)
+            {
+                Owner.Location = new Point((Owner.Location.X), (Owner.Location.Y - 5));
+            }
+            if (f_down)
+            {
+                Owner.Location = new Point((Owner.Location.X), (Owner.Location.Y + 5));
+            }
+
+        }
+
+
+
+        private void right_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Start();
+            f_right=true;
+            Owner.Location = new Point((Owner.Location.X + 5), (Owner.Location.Y));
+        }
+
+        private void down_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Start();
+            f_down = true;
+            Owner.Location = new Point((Owner.Location.X), (Owner.Location.Y + 5));
+        }
+
+        private void up_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Start();
+            f_up= true;
+            Owner.Location = new Point((Owner.Location.X), (Owner.Location.Y - 5));
+        }
+
+        private void left_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Start();
+            f_left = true;
+            Owner.Location = new Point((Owner.Location.X - 5), (Owner.Location.Y));
+        }
+        
+        private void right_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+            f_up = false;
+            f_down = false;
+            f_right = false;
+            f_left = false;
+            Properties.Settings.Default.X = Owner.Location.X;
+            Properties.Settings.Default.Y = Owner.Location.Y;
         }
     }
 }
